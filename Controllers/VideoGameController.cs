578c2123559a7d0365eb9d81169451d2b4906e25
yunus <yunus@YUNUSKAPTAN>
2025-05-29 14:39:01 +0200

    Add project files.

---
+++ Controllers/VideoGameController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+        static private List<VideoGame> videoGames = new List<VideoGame> 
+        {
+            new VideoGame
+            {
+                Id = 1,
+                Title = "Spider-Man 2",
+                Platform = "PS5",
+                Developer = " Insomniac Games",
+                Publisher = "Sony"  
+            },
+            new VideoGame
+            {
+                Id = 2,
+                Title = "The Legend of Zelda: Breath of the wild",
+                Platform = "Nintendo Switch",
+                Developer = "Nintendo EPD",
+                Publisher = "Nintendo"
+            },
+            new VideoGame
+            {
+                Id = 3,
+                Title = "Cyberpunk 2077",
+                Platform = "PC",
+                Developer = "CD Projekt Red",
+                Publisher = "CD Projekt"
+            }
+        };
+        [HttpGet]
+        public ActionResult<List<VideoGame>> GetVideoGames()
+        {
+            return Ok(videoGames);
+        }
+    }
+}
