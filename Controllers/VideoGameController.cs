56bdc42c3e89b29a43272e4d02b308f4d611789f
YunusKaptan <yunuskaptanofficial>
2025-05-30 14:11:02 +0200

    Generated Delete and Update functions

--- Controllers/VideoGameController.cs
+++ Controllers/VideoGameController.cs
@@ -56,7 +56,32 @@ namespace VideoGameApi.Controllers
 
             newGame.Id = videoGames.Max(g => g.Id) + 1;
             videoGames.Add(newGame);
-            return CreatedAtAction(nameof(GetVideoGameById), new { id= newGame.Id }, newGame); 
+            return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UptadeVideoGame(int id, VideoGame updatedGame)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+            if (game is null)
+                return NotFound();
+
+            game.Title = updatedGame.Title;
+            game.Platform = updatedGame.Platform;
+            game.Developer = updatedGame.Developer;
+            game.Publisher = updatedGame.Publisher;
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteVideoGame(int id)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+            if (game is null)
+                return NotFound();
+
+            videoGames.Remove(game);
+            return NoContent();
 
         }
     }
